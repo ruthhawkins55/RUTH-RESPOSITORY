85ca5a04d3d98cda4589ca69cd6df3206a59f695
ruthhawkins55 <ruthhawkins55@users.noreply.github.com>
2025-01-25 21:14:09 +0000

    deploy: 8500631d6c1503f17437aa0b4230ffba82f6475b

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import js from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,  // Maintain the browser global context if needed
+    },
+    rules: {
+      "no-unused-vars": "warn",  // Warn about unused variables
+      "no-undef": "warn",        // Warn about undefined variables
+    },
+    files: ["*.js"],  // Apply these rules to JavaScript files
+  },
+];
+
